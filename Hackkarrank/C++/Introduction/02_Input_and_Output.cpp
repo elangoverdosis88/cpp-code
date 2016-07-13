f41763db6ab26805547e59f3f962cc7c7a5886cc
champika <pgsamila@yahoo.com>
2016-07-14 01:04:11 +0530

    creating files

---
+++ Hackkarrank/C++/Introduction/02_Input_and_Output.cpp
@@ -0,0 +1,18 @@
+//
+// Created by pgsamila on 7/14/16.
+//
+
+#include <istream>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int int1,int2,int3;
+
+    cin >> int1 >> int2 >> int3;
+    int1 = int1 + int2 + int3;
+    cout << int1 << endl;
+    return 0;
+}
+
