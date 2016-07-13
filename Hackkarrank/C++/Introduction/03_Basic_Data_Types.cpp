9e6a603ee3cff9781d62b429784e24d79ef4ada8
champika <pgsamila@yahoo.com>
2016-07-14 01:05:25 +0530

    creating files

--- Hackkarrank/C++/Introduction/03_Basic_Data_Types.cpp
+++ Hackkarrank/C++/Introduction/03_Basic_Data_Types.cpp
@@ -3,18 +3,19 @@
 //
 
 #include <iostream>
-#include <climits>
 
 using namespace std;
 
 int main(){
     int int1;
     long long1;
-    long long int long2;
+    long long long2;
     char char1;
     float float1;
     double double1;
 
     cin >> int1 >> long1 >> long2 >> char1>> float1 >> double1;
-    cout << int1 << endl << long1 << endl << long2 << endl << char1 << endl << float1 << endl << double1 << endl;
+    cout << int1 << endl << long1 << endl << long2 << endl;
+    cout << char1 << endl << float1 << endl << double1 << endl;
+    return 0;
 }
