fd7b596e01c99e222f5742fd5f96c829db2eb4a5
champika <pgsamila@yahoo.com>
2016-07-14 13:29:00 +0530

    hackarrank solutions

---
+++ Hackkarrank/C++/Introduction/04_Conditional_Statements.cpp
@@ -0,0 +1,36 @@
+//
+// Created by ghost on 7/14/16.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i;
+    cin >> i;
+
+    if(i>9 || i<1){
+        cout <<"Greater than 9"<<endl;
+    } else{
+        if(i==1){
+            cout << "one"<<endl;
+        }else if(i==2){
+            cout << "two"<< endl;
+        }else if(i==3){
+            cout << "three"<< endl;
+        }else if(i==4){
+            cout << "four" << endl;
+        }else if(i==5){
+            cout << "five" << endl;
+        }else if(i==6){
+            cout << "six" << endl;
+        }else if(i==7){
+            cout << "seven"<< endl;
+        }else if(i==8){
+            cout << "eight"<< endl;
+        }else if(i==9){
+            cout << "nine" << endl;
+        }
+    }
+}
