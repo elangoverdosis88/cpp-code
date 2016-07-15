cfd004e66bee74cafcffa51809d9c76829f098f9
champika <pgsamila@yahoo.com>
2016-07-15 19:32:23 +0530

    Problem 13 incomplete!

---
+++ Hackkarrank/C++/Introduction/13_Virtual_Functions.cpp
@@ -0,0 +1,46 @@
+//
+// Created by ghost on 7/15/16.
+//
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+//********************************************************88
+//  My Code
+class Person {
+
+};
+
+//  Org code
+//**********************************************************
+
+int main(){
+
+    int n, val;
+    cin>>n; //The number of objects that is going to be created.
+    Person *per[n];
+
+    for(int i = 0;i < n;i++){
+
+        cin>>val;
+        if(val == 1){
+            // If val is 1 current object is of type Professor
+            per[i] = new Professor;
+
+        }
+        else per[i] = new Student; // Else the current object is of type Student
+
+        per[i]->getdata(); // Get the data from the user.
+
+    }
+
+    for(int i=0;i<n;i++)
+        per[i]->putdata(); // Print the required output for each object.
+
+    return 0;
+
+}
